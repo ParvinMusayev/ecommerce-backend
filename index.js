86f1a00f412b7761f3f9f759f79bdc4c496f75c9
ParvinMusayev <parvinmusayev1@gmail.com>
2024-04-10 15:56:02 +0400

    feat: Add auth router

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const dbConnect = require("./config/dbConnect");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
+const authRouter = require("./routes/authRoute");
 
 dbConnect();
 
+app.use("/api/user", authRouter);
 app.use("/", (req, res) => {
   res.send("Hello from server side");
 });
