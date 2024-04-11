74c5faf3133cdfbea8b966a614e02328d96e111c
ParvinMusayev <parvinmusayev1@gmail.com>
2024-04-11 17:05:56 +0400

    feat: Add password bcrypt

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,17 +1,15 @@
 const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
 
-const createUser = async (req, res) => {
+const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
   const findUser = await User.findOne({ email: email });
   if (!findUser) {
     const newUser = await User.create(req.body);
     res.json(newUser);
   } else {
-    res.json({
-      msg: "User Already Exists",
-      success: false,
-    });
+    throw new Error("User Already Exists");
   }
-};
+});
 
 module.exports = { createUser };
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   firstname: {
@@ -24,5 +25,17 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSaltSync(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
