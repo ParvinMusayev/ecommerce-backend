db1d9a65c76f5367105f27cbe45f0480ffc6196c
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-07 20:49:29 +0400

    fixes

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,5 +1,6 @@
 const Product = require("../models/productModel");
 const asyncHandler = require("express-async-handler");
+const slugify = require("slugify");
 
 // create product
 const createProduct = asyncHandler(async (req, res) => {
