b40eecda4435ebf6fb47b5819a8358af2a6665c6
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-07 19:39:05 +0400

    feat: Add product route

---
+++ routes/productRoute.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { createProduct } = require("../controller/productCtrl");
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
+const router = express.Router();
+
+router.post("/", authMiddleware, isAdmin, createProduct);
+
+module.exports = router;
