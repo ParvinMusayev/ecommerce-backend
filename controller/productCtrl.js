48d7742d5985c6e99790b82b007af00fe259a390
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-07 18:44:06 +0400

    feat: Add product controllers

---
+++ controller/productCtrl.js
@@ -0,0 +1,19 @@
+const Product = require("../models/productModel");
+const asyncHandler = require("express-async-handler");
+
+// create product
+const createProduct = asyncHandler(async (req, res) => {
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+    const newProduct = await Product.create(req.body);
+    res.json(newProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = {
+  createProduct,
+};
