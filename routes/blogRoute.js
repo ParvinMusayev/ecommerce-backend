19995253c0d8ece948060e429f00063945cfd6f2
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-09 03:01:41 +0400

    feat: Add like blog function

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -5,11 +5,14 @@ const {
   getBlog,
   getAllBlogs,
   deleteBlog,
+  liketheBlog,
 } = require("../controller/blogCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createBlog);
+router.put("/likes", authMiddleware, liketheBlog);
+
 router.put("/:id", authMiddleware, isAdmin, updateBlog);
 router.get("/:id", getBlog);
 router.get("/", getAllBlogs);
