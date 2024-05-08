1e4bc8cff867aa5b5147e5ff960b1690a5dce583
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-09 02:53:52 +0400

    feat: Add getBlog function

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -27,4 +27,24 @@ const updateBlog = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { createBlog, updateBlog };
+const getBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const getBlog = await Blog.findById(id)
+      .populate("likes")
+      .populate("dislikes");
+    const updateViews = await Blog.findByIdAndUpdate(
+      id,
+      {
+        $inc: { numViews: 1 },
+      },
+      { new: true }
+    );
+    res.json(getBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { createBlog, updateBlog, getBlog };
