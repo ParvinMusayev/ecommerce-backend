9b12a7fbf311752a8140c5f7062f3a392c155639
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-09 02:35:50 +0400

    feat: Add reset password function

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -228,6 +228,23 @@ const forgotPasswordToken = asyncHandler(async (req, res) => {
   }
 });
 
+// reset password
+const resetPassword = asyncHandler(async (req, res) => {
+  const { password } = req.body;
+  const { token } = req.params;
+  const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+  if (!user) throw new Error(" Token Expired, Please try again later");
+  user.password = password;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+  res.json(user);
+});
+
 module.exports = {
   createUser,
   loginUserCtrl,
@@ -241,4 +258,5 @@ module.exports = {
   logout,
   updatePassword,
   forgotPasswordToken,
+  resetPassword,
 };
