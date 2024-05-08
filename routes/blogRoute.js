1bf1f252a9755965e3687e0b7c1a6f82906390a4
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-09 02:54:49 +0400

    feat: Add getAll blogs function

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { createBlog, updateBlog, getBlog } = require("../controller/blogCtrl");
+const {
+  createBlog,
+  updateBlog,
+  getBlog,
+  getAllBlogs,
+} = require("../controller/blogCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createBlog);
 router.put("/:id", authMiddleware, isAdmin, updateBlog);
 router.get("/:id", getBlog);
+router.get("/", getAllBlogs);
 
 module.exports = router;
