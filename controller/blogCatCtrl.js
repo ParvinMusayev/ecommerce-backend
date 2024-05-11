3a6638109313b750583aa4d3c6ee0171ba16f5dd
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-12 00:05:55 +0400

    feat: add delete category function

--- controller/blogCatCtrl.js
+++ controller/blogCatCtrl.js
@@ -24,6 +24,19 @@ const updateCategory = asyncHandler(async (req, res) => {
   }
 });
 
+const deleteCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const deletedCategory = await Category.findByIdAndDelete(id);
+    res.json(deletedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 module.exports = {
   createCategory,
+  updateCategory,
+  deleteCategory,
 };
