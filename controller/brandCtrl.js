00e865dff0bf0e82b6bfb76d64e98070c3e16e10
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-12 00:25:07 +0400

    feat: Add all brand functions

--- controller/brandCtrl.js
+++ controller/brandCtrl.js
@@ -35,8 +35,30 @@ const deleteBrand = asyncHandler(async (req, res) => {
   }
 });
 
+const getBrand = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const getaBrand = await Brand.findById(id);
+    res.json(getaBrand);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getallBrand = asyncHandler(async (req, res) => {
+  try {
+    const getallBrand = await Brand.find();
+    res.json(getallBrand);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 module.exports = {
   createBrand,
   updateBrand,
   deleteBrand,
+  getBrand,
+  getallBrand,
 };
