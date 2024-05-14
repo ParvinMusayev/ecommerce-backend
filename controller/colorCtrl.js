9fe4ed69babe7d80c108347471b6ea7a8a91cad3
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-14 22:00:15 +0400

    feat: Add color function for controller

--- controller/colorCtrl.js
+++ controller/colorCtrl.js
@@ -24,7 +24,19 @@ const updateColor = asyncHandler(async (req, res) => {
   }
 });
 
+const getColor = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const getaColor = await Color.findById(id);
+    res.json(getaColor);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 module.exports = {
   createColor,
   updateColor,
+  getColor,
 };
--- routes/colorRoute.js
+++ routes/colorRoute.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { createColor, updateColor } = require("../controller/colorCtrl");
+const {
+  createColor,
+  updateColor,
+  getColor,
+} = require("../controller/colorCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createColor);
 router.put("/:id", authMiddleware, isAdmin, updateColor);
-
+router.get("/:id", getColor);
 module.exports = router;
