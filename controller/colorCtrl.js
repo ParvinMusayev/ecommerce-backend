e1edf415102b83f25cd429fc1e083207511ee35b
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-14 21:50:01 +0400

    feat: Add color controller

---
+++ controller/colorCtrl.js
@@ -0,0 +1,16 @@
+const Color = require("../models/colorModel");
+const asyncHandler = require("express-async-handler");
+const validateMongoDbId = require("../utils/validateMongodbId");
+
+const createColor = asyncHandler(async (req, res) => {
+  try {
+    const newColor = await Color.create(req.body);
+    res.json(newColor);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = {
+  createColor,
+};
