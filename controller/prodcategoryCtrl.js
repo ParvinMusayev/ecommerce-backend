65eb52fe2ca9c8638b1b0d436a9cf347dc8528d0
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-16 20:56:24 +0400

    chore: update controller

--- controller/prodcategoryCtrl.js
+++ controller/prodcategoryCtrl.js
@@ -11,6 +11,20 @@ const createCategory = asyncHandler(async (req, res) => {
   }
 });
 
+const updateCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 module.exports = {
   createCategory,
+  updateCategory,
 };
